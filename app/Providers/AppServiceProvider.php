bfa8e3c60a83f84d575b4cae3404a551d4c7eaf6
AngelloOZ <angelo-mjz7@hotmail.com>
2023-10-19 14:54:53 -0500

    config: Spatie roles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +21,21 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Schema::defaultStringLength(191);
+
+        // Inertia::share('loggedUser', function () {
+
+        //     if (!Auth::check()) return null;
+
+        //     $userLogged = User::find(Auth::user()->id);
+
+        //     if (!$userLogged) return Auth::user();
+        //     unset($userLogged->email_verified_at, $userLogged->roles, $userLogged->created_at); 
+
+        //     $userLogged->listRols = $userLogged->getRoleNames();
+        //     $userLogged->listPermissions = $userLogged->getPermissionsViaRoles()->pluck('name');
+
+        //     return $userLogged;
+        // });
     }
 }
